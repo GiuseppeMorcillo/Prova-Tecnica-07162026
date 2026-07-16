2b516aae82a740d9934bae48402b2e7f0999abfa
giuseppemorcillo <beppebeppe18@gmail.com>
2026-07-16 18:15:14 +0200

    create scaffholding per il progetto

---
+++ weather-dashboard/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     return <h1> Weather Dashboard </h1>
+    </>
+  )
+}
+
+export default App
